af5b80b0ce92a9b6de88b411023a66eb2bf82e36
Jordan Jennings <jordan.jennings@mystenlabs.com>
2024-09-04 17:48:06 -0700

    working equivocation poc, no need for function async works in top scope

--- src/equivocation.js
+++ src/equivocation.js
@@ -1,48 +1,24 @@
-import {Deploy, Faucet, client, keypair} from './utils.js';
+import {Faucet, client, keypair} from './utils.js';
 import { Transaction } from '@mysten/sui/transactions';
 
+await Faucet();
+let parallelTxs = []
 
-async function getCoins() {
-    let address_str = "0xccb8a90ff6ede2012b865873213eb56e6ac5f226a436a7e89965ef94e42fbbca";
-    let address = Buffer.from(address_str, 'hex');
-    let coins = await client.getCoins({
-        owner: address_str,
-    });
-
-    if (coins.length == 0) {
-        await Faucet();
-        coins = await client.getCoins(keypair.publicKey);
-    }
-    console.log(coins);
-
-    return coins;
-}
-
-async function run() {
-    await Faucet();
-
+for (var i = 0; i < 5; i++) {
     let tx = new Transaction();
-    const [coin] = tx.splitCoins(tx.gas, [100]);
+    const [coin] = tx.splitCoins(tx.gas, [i+1]);
 
     // transfer the split coin to a specific address
     tx.transferObjects([coin], '0xccb8a90ff6ede2012b865873213eb56e6ac5f226a436a7e89965ef94e42fbbca');
 
-    let parallelTxs = []
-
-    for (var i = 0; i < 20; i++) {
-        parallelTxs.push(client.signAndExecuteTransaction({
-            transaction: tx,
-            signer: keypair,
-        }));
-    }
-
-    console.log(`sending the same tx multiple ${parallelTxs.length} times`);
-    await Promise.all(parallelTxs)
+    parallelTxs.push(client.signAndExecuteTransaction({
+        transaction: tx,
+        signer: keypair,
+    }));
 }
 
+console.log(`sending ${parallelTxs.length} conflicting TXs`);
+const result =  await Promise.all(parallelTxs)
 
 console.log(`running equivocation test`)
-
-
-// await getCoins();
-await run();
+console.log(result);
